4b56991489bfac63a07150d31e0661fc08c02cbf
Naomy Chepngeno <85355678+Naomy360@users.noreply.github.com>
2025-08-14 23:34:06 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://frontend:3000"
+        "http://frontend:3000",
+        "https://steady-macaron-535e2c.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
